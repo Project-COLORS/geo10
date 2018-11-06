aaf4dd1bba0b4dacfae53092fe3699f16a8cf1de
risonakamo <hmykanae@gmail.com>
2018-11-06 01:40:58 -0500

    remade cursor

--- Assets/cursor/cursor.cs
+++ Assets/cursor/cursor.cs
@@ -4,7 +4,22 @@ using UnityEngine;
 
 public class cursor:MonoBehaviour
 {
-    public GameObject _cam;
+    /*-- externals --*/
+    public GameObject _cam; //set to maincam
+    public Rigidbody _body; //set to SELF
+
+    /*-- camera and movement related --*/
+    Vector3 _moveVec=new Vector3();
+    Vector3 t_moveVec=new Vector3();
+    Vector3 _posvec=new Vector3();
+
+    Vector3 _camAngle=new Vector3(30,0,0);
+    readonly float c_cursorspeed=7.9f;
+    readonly int [,] c_camPositions=new int[4,7]{{1,1,225,0,2,-1,1},{-1,1,135,2,0,-1,-1},{-1,-1,45,0,2,1,-1},{1,-1,-45,2,0,1,1}};
+
+    float _targetcamYAngle=225f;
+    int _currentcamPosition=0;
+    float[] _camPositionsCurrent=new float[3]{0,0,0};
 
     void Start()
     {
@@ -15,5 +30,68 @@ public class cursor:MonoBehaviour
     {
         //set cursor always point to camera
         transform.forward=-_cam.transform.forward;
+
+        inputProcessing();
+        positionUpdate();
+    }
+
+    void inputProcessing()
+    {
+        _moveVec.x=Input.GetAxisRaw("Vertical");
+        _moveVec.z=Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("rotateleft"))
+        {
+            _targetcamYAngle-=90;
+            if (_currentcamPosition>=3)
+            {
+                _currentcamPosition=0;
+            }
+
+            else
+            {
+                _currentcamPosition++;
+            }
+        }
+
+        else if (Input.GetButtonDown("rotateright"))
+        {
+            _targetcamYAngle+=90;
+            if (_currentcamPosition<=0)
+            {
+                _currentcamPosition=3;
+            }
+
+            else
+            {
+                _currentcamPosition--;
+            }
+        }
+    }
+
+    void positionUpdate()
+    {
+        //performing cursor movement, with adjustments based on the current camera position
+        _moveVec.Normalize();
+        t_moveVec.x=_moveVec[c_camPositions[_currentcamPosition,3]]*c_cursorspeed*c_camPositions[_currentcamPosition,5];
+        t_moveVec.z=_moveVec[c_camPositions[_currentcamPosition,4]]*c_cursorspeed*c_camPositions[_currentcamPosition,6];
+        t_moveVec=Quaternion.Euler(0,-45,0)*t_moveVec;
+        _body.velocity=t_moveVec;
+
+        //performing camera rotations, based on the target cam angle
+        _camPositionsCurrent[0]=Mathf.Lerp(_camPositionsCurrent[0],5*c_camPositions[_currentcamPosition,0],.1f);
+        _camPositionsCurrent[1]=Mathf.Lerp(_camPositionsCurrent[1],5*c_camPositions[_currentcamPosition,1],.1f);
+        _camPositionsCurrent[2]=Mathf.Lerp(_camPositionsCurrent[2],_targetcamYAngle,.1f);
+
+        //performing camera position movement
+        _posvec=transform.position;
+        _posvec.x+=_camPositionsCurrent[0];
+        _posvec.y+=4;
+        _posvec.z+=_camPositionsCurrent[1];
+
+        _camAngle.y=_camPositionsCurrent[2];
+
+        _cam.transform.eulerAngles=_camAngle;
+        _cam.transform.position=_posvec;
     }
 }
