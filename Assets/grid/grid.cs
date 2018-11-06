b93d77ef19bc7fab2a5c54c257dee664bd2437c9
risonakamo <hmykanae@gmail.com>
2018-11-06 12:47:47 -0500

    more experiments with grid

--- Assets/grid/grid.cs
+++ Assets/grid/grid.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class grid:MonoBehaviour
 {
+    public GameObject r_tile1; //set to floating tile object
+    public Grid r_grid; //set to self
+
+    float c_tileDimension=1.0098f; //the physical width/height of a tile
+    Vector3 c_gridSpawnOffset=new Vector3(); //initialised later to half of the tile dimension,
+                                             //so things spawn in the middle
+
+    int[] c_gridDimension=new int[2]{10,10}; //width/height of the map, in number of tiles,
+                                             //not physical dimension
+
     void Start()
     {
+        c_gridSpawnOffset.Set(c_tileDimension/2,0,c_tileDimension/2);
 
+        for (int x=0;x<c_gridDimension[0];x++)
+        {
+            for (int y=0;y<c_gridDimension[1];y++)
+            {
+                Instantiate(r_tile1,r_grid.CellToWorld(new Vector3Int(x,y,0))+c_gridSpawnOffset,Quaternion.Euler(-90,0,0));
+            }
+        }
     }
 }
