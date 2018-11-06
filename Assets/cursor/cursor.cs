a4cefe9a98d332849ed741513713377400326beb
risonakamo <hmykanae@gmail.com>
2018-11-06 01:17:43 -0500

    beginning cursor shenanigans

---
+++ Assets/cursor/cursor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursor:MonoBehaviour
+{
+    public GameObject _cam;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //set cursor always point to camera
+        transform.forward=-_cam.transform.forward;
+    }
+}
