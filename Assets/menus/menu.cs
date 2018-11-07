b822e720c9e534b83faac7703572599d357af14a
risonakamo <hmykanae@gmail.com>
2018-11-07 13:33:48 -0500

    setting up menu system

---
+++ Assets/menus/menu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class menu:MonoBehaviour
+{
+    menuitem[] _menuItems;
+
+    void Start()
+    {
+        _menuItems=transform.GetComponentsInChildren<menuitem>();
+
+        setActionMenu(new string[]{"a"},new System.Action[2]);
+    }
+
+    void setActionMenu(string[] menuTexts,System.Action[] menuActions)
+    {
+        for (int x=0;x<_menuItems.Length;x++)
+        {
+            if (x>=menuTexts.Length)
+            {
+                _menuItems[x].setActive(false);
+            }
+
+            else
+            {
+                _menuItems[x].setActive(true);
+                _menuItems[x].setItem(menuTexts[x]);
+            }
+        }
+    }
+}
---
+++ Assets/menus/menuitem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class menuitem:MonoBehaviour
+{
+    public Text thetext;
+    public System.Action itemAction;
+
+    public void setItem(string itemText,System.Action newItemAction=null)
+    {
+        thetext.text=itemText;
+        itemAction=newItemAction;
+    }
+
+    public void setActive(bool active)
+    {
+        this.gameObject.SetActive(active);
+    }
+}
